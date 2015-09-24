91bf48a2726fff88b4a5f8e533d960e3d699580c
ERNEST SON <sunnee5150@gmail.com>
2015-09-23 20:36:22 -0400

    all bower and dev dependencies installed

---
+++ server.js
@@ -0,0 +1,11 @@
+(function() {
+	"use strict";
+	var express = require('express');
+	var bodyParser = require ('body-parser');
+	var server = express();
+
+	server.use(express.static('public'));
+	server.listen(9000);
+
+	console.log('the server is running');
+}())
