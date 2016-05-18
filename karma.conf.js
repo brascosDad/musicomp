f431d604b667772c3b30d3c6f54b6b239bbd0387
ERNEST SON <sunnee5150@gmail.com>
2016-05-18 15:41:27 -0400

    cleaned up karma.conf, gulp test, gulp _globals but still getting phantomjs parse error in globalErrorHandler

--- karma.conf.js
+++ karma.conf.js
@@ -13,61 +13,16 @@ files = _.union(libFiles, appFiles);
 
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    //basePath: '',
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
-    // list of files / patterns to load in the browser
     files: files,
-
-    // list of files to exclude
-    //exclude: [
-    //],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    // preprocessors: {
-    //     ".src/**/!(*.spec).js": ["coverage"]
-    // },
-
-    // coverageReporter: {
-    //     type: 'lcovonly' //change from text to text-summary for details or summary
-    // },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    preprocessors: {
+        ".src/**/!(*.spec).js": ["coverage"]
+    },
+    coverageReporter: {
+        type: 'lcovonly' //change from text to text-summary for details or summary
+    },
     reporters: ['dots', 'coverage'],
-
-    // web server port
-    //port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
-    //colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    //logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
-    //autoWatch: true,
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS']
-
-    // plugins: [
-    //     'karma-coverage'
-    // ]
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    //singleRun: false
-
   });
 
   config.LOG_DEBUG = true;
